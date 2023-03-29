4e4a3de5f67c11f4d32f3f04333ca2b05dcf0d3a
onurtalayhan <onur.talayhan@gmail.com>
2023-03-29 19:36:36 +0200

    added base, contact and index files

--- MarketPlace/urls.py
+++ MarketPlace/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from core.views import index
+from core.views import index, contact
 
 urlpatterns = [
-    path('', index, name='Index'),
+    path('', index, name='index'),
+    path('contact/', contact, name="contact"),
     path('admin/', admin.site.urls),
 ]
--- core/views.py
+++ core/views.py
@@ -4,5 +4,5 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'core/index.html')
 
-def contact(request)
+def contact(request):
     return render(request, 'core/contact.html')
